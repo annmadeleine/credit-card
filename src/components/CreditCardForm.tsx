3cbb0fbf56344872c918ad2f2e8782394602573d
Madeleine Olson <madeleineolson@outlook.com>
2021-03-11 12:04:33 +0100

    Update

--- src/components/CreditCardForm.tsx
+++ src/components/CreditCardForm.tsx
@@ -1,46 +1,99 @@
 import './CreditCardForm.scss';
 import React from 'react';
+import { Formik, Form, Field, FormikHelpers } from 'formik';
+
+interface CreditCardProps {
+  cardNumber: number;
+  cardName: string;
+  cardExpiration: number;
+  cardCVV: number;
+}
+
+const initialValues: CreditCardProps = {
+  cardNumber: undefined,
+  cardName: '',
+  cardExpiration: undefined,
+  cardCVV: undefined,
+};
 
 const CreditCardForm: React.FC = () => {
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const handleOnSubmit = (
+    values: CreditCardProps,
+    { setSubmitting }: FormikHelpers<CreditCardProps>
+  ) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 500);
+  };
 
   return (
-    <div className="credit-card-form">
-      <form className="credit-card-form__form">
-        <label className="credit-card-form__label">Card Number</label>
-        <input
-          className="credit-card-form__input"
-          type="number"
-          value={cardNumber}
-          name="cardNumber"
-          onChange={handleInputChange}
-        />
-        <label className="credit-card-form__label">
-          Card Name {creditCard.cardNumber}
-        </label>
-        <input
-          className="credit-card-form__input credit-card__name"
-          type="text"
-        />
-        <div className="credit-card-form__group">
-          <div className="credit-card-form__child">
-            <label className="credit-card-form__label">Expiration Date</label>
-            <div className="credit-card-form__double">
-              <input className="credit-card-form__input" type="number" />
-              <input className="credit-card-form__input" type="number" />
-            </div>
-          </div>
-          <div className="credit-card-form__child">
-            <label className="credit-card-form__label">CVV</label>
-            <input className="credit-card-form__input" type="number" />
-          </div>
-        </div>
-        <input
-          className="credit-card-form__button"
-          type="submit"
-          value="Submit"
-        />
-      </form>
+    <div>
+      <div className="credit-card-form">
+        <Formik initialValues={initialValues} onSubmit={handleOnSubmit}>
+          {({ values, handleSubmit }) => {
+            return (
+              <Form className="credit-card-form__form" onSubmit={handleSubmit}>
+                <label htmlFor="cardNumber" className="credit-card-form__label">
+                  Card Number
+                </label>
+                <Field
+                  id="cardNumber"
+                  name="cardNumber"
+                  className="credit-card-form__input"
+                  type="number"
+                  value={values.cardNumber}
+                />
+                <label htmlFor="cardName" className="credit-card-form__label">
+                  Card Name
+                </label>
+                <Field
+                  id="cardName"
+                  name="cardName"
+                  className="credit-card-form__input credit-card__name"
+                  type="text"
+                  value={values.cardName}
+                />
+                <div className="credit-card-form__group">
+                  <div className="credit-card-form__child">
+                    <label
+                      htmlFor="cardExpiration"
+                      className="credit-card-form__label"
+                    >
+                      Expiration Date
+                    </label>
+                    <Field
+                      id="cardExpiration"
+                      name="cardExpiration"
+                      className="credit-card-form__input"
+                      type="number"
+                      value={values.cardExpiration}
+                    />
+                  </div>
+                  <div className="credit-card-form__child">
+                    <label
+                      htmlFor="cardCVV"
+                      className="credit-card-form__label"
+                    >
+                      CVV
+                    </label>
+                    <Field
+                      id="cardCVV"
+                      name="cardCVV"
+                      className="credit-card-form__input"
+                      type="number"
+                      value={values.cardCVV}
+                    />
+                  </div>
+                </div>
+                <button className="credit-card-form__button" type="submit">
+                  Submit
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </div>
     </div>
   );
 };
